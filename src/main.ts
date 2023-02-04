d14b8e1f2cfeb960ec7ffd8e1241fad8de8e565e
Caroline Martins <53910116+CarolineBarbosaM@users.noreply.github.com>
2023-02-04 13:45:31 -0300

    Fix: ajustes no projeto

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
 import { BadRequestException } from '@nestjs/common';
 import { ValidationPipe } from '@nestjs/common/pipes';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User management')
+    .setDescription('User management API description')
+    .setVersion('1.0')
+    .addTag('Usuários')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe ({
     transform: true,
     exceptionFactory: (erros)=> new BadRequestException(erros)
   }));
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
+
 }
 bootstrap();
